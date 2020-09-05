3888ed34bca2165ee08418fc36af87ef66b3b069
hinakawa <wsk.ankit1k@gmail.com>
2020-09-05 12:31:30 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  get 'login', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
