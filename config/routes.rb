ed5cc7f34cc0c6c085a2120f24f3ef818bda93f9
hinakawa <wsk.ankit1k@gmail.com>
2020-09-16 12:51:00 +0000

    favorite

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
+  get 'create/destroy'
   get 'relationships/create'
   get 'relationships/destroy'
   get 'microposts/create'
@@ -16,17 +19,19 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'favorites', to: 'favorites#create'
+  delete 'unfavorite', to: 'favorites#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create] do
     member do
       get :followings
       get :followers
+      get :likes
     end 
-    collection do
-      get :search
-    end
+   
   end 
+  resources :favorites, only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
   resources :microposts, only: [:create, :destroy]
 end
